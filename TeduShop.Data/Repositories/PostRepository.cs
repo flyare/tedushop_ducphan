eb05622052ce7fd8df392f377d5c30c31e412ada
Duc Phan <flyare@gmail.com>
2016-07-29 23:53:26 +0700

    start build service layer

--- TeduShop.Data/Repositories/PostRepository.cs
+++ TeduShop.Data/Repositories/PostRepository.cs
@@ -3,7 +3,11 @@ using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    public class PostRepository : RepositoryBase<Post>
+    public interface IPostRepository: IRepository<Post>
+    {
+    }
+
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
     {
         public PostRepository(IDbFactory dbFactory) : base(dbFactory)
         {
