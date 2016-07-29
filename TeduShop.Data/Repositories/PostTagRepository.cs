9228c9b70f0e5da920286d6201bba77d2329930f
Duc Phan <flyare@gmail.com>
2016-07-29 22:53:47 +0700

    Add repository

---
+++ TeduShop.Data/Repositories/PostTagRepository.cs
@@ -0,0 +1,12 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public class PostTagRepository : RepositoryBase<PostTag>
+    {
+        public PostTagRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ TeduShop.Data/Repositories/TagRepository.cs
@@ -0,0 +1,12 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public class TagRepository : RepositoryBase<Tag>
+    {
+        public TagRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
