c0f9c718684884c1e57323ebc4c35da0d17a16e4
ducphan <ducphan@DUCPHAN-PC>
2016-08-10 23:15:48 +0700

    doing api

---
+++ TeduShop.Web/Api/ProductCategoryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+using TeduShop.Web.Infrastructure.Core;
+
+namespace TeduShop.Web.Api
+{
+    [Route("api/productcategory")]
+    public class ProductCategoryController : ApiControllerBase
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService) : base(errorService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        //[Route("getall")] public HttpResponseMessage GetAll(HttpRequestMessage request)
+        //{
+        //    return CreateHttpResponse(request, () =>
+        //    {
+        //        var model = _productCategoryService.GetAll();
+
+        //        var response = Mapper.Map<List<ProductCategory>, List<ProductCategorViewModel>>(model);
+
+        //        return response;
+        //    });
+        //}
+    }
+}
