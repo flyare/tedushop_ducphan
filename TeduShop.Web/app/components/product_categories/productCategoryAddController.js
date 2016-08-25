a0ae7c1014b518e87d8b27fc2eaf86ce815d693f
ducphan <ducphan@DUCPHAN-PC>
2016-08-25 10:47:38 +0700

    fix bug

--- TeduShop.Web/app/components/product_categories/productCategoryAddController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryAddController.js
@@ -6,7 +6,9 @@
     function productCategorytAddController($scope, apiService, notificationService, $state) {
         $scope.productCategory = {
             CreatedDate: new Date(),
-            Status: true
+            Status: true,
+            Name: "Danh muc 1",
+            Alias: "Danh-muc-1"
         };
 
         $scope.parentCategories = [];
@@ -17,7 +19,8 @@
             apiService.post("/api/productcategory/create", $scope.productCategory, function(result) {
                 notificationService.displaySuccess(result.data.Name + " đã được thêm mới.");
                 $state.go("product_categories");
-            }, function(error) {
+            }, function (error) {
+                console.log(error);
                 notificationService.displayError("Thêm mới không thành công.");
             });
         }
